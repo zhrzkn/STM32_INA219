bbe04f347c580367d1a08b78661147963b983702
Zehra <78693714+zhrzkn@users.noreply.github.com>
2021-09-01 17:03:12 +0300

    Add files via upload

---
+++ INA219_Register.h
@@ -0,0 +1,20 @@
+/*
+ * INA219_Register.h
+ *
+ *  Created on: 1 Eyl 2021
+ *      Author: Uranus
+ */
+
+#ifndef INC_INA219_REGISTER_H_
+#define INC_INA219_REGISTER_H_
+
+
+#define Configuration  0x00
+#define Shunt_Voltage  0x01
+#define Bus_Voltage    0x02
+#define Power          0x03
+#define Current        0x04
+#define Calibration    0x05
+
+
+#endif /* INC_INA219_REGISTER_H_ */
